9b60e48d9713bb504e06bedd650a985197a474bd
oo00oo00oo00 <oo00oo00oo00@tripleten.com>
2025-06-06 17:01:08 +0200

    better

--- lua/oo00oo00oo00/plugins/lsp.lua
+++ lua/oo00oo00oo00/plugins/lsp.lua
@@ -20,7 +20,8 @@ return { -- LSP Configuration & Plugins
 				require("lsp_signature").setup(opts)
 			end,
 		},
-
+		-- Allows extra capabilities provided by blink.cmp
+		"saghen/blink.cmp",
 	},
 	config = function()
 		--  This function gets run when an LSP attaches to a particular buffer.
@@ -35,7 +36,7 @@ return { -- LSP Configuration & Plugins
 					vim.keymap.set("n", keys, func, { buffer = event.buf })
 				end
 
-                map("<leader>cd", vim.diagnostic.open_float)
+				map("<leader>cd", vim.diagnostic.open_float)
 				map("gd", require("telescope.builtin").lsp_definitions)
 				map("gr", require("telescope.builtin").lsp_references)
 				map("gI", require("telescope.builtin").lsp_implementations)
@@ -80,6 +81,7 @@ return { -- LSP Configuration & Plugins
 				end
 			end,
 		})
+
 		vim.api.nvim_create_autocmd("LspDetach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
 			callback = function(event)
@@ -88,12 +90,40 @@ return { -- LSP Configuration & Plugins
 			end,
 		})
 
+		-- Diagnostic Config
+		-- See :help vim.diagnostic.Opts
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { border = "rounded", source = "if_many" },
+			underline = { severity = vim.diagnostic.severity.ERROR },
+			signs = vim.g.have_nerd_font and {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+			} or {},
+			virtual_text = {
+				source = "if_many",
+				spacing = 2,
+				format = function(diagnostic)
+					local diagnostic_message = {
+						[vim.diagnostic.severity.ERROR] = diagnostic.message,
+						[vim.diagnostic.severity.WARN] = diagnostic.message,
+						[vim.diagnostic.severity.INFO] = diagnostic.message,
+						[vim.diagnostic.severity.HINT] = diagnostic.message,
+					}
+					return diagnostic_message[diagnostic.severity]
+				end,
+			},
+		})
+
 		-- LSP servers and clients are able to communicate to each other what features they support.
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -105,9 +135,18 @@ return { -- LSP Configuration & Plugins
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			-- clangd = {},
 			gopls = {},
-			pyright = {},
+			pyright = {
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = false,
+							diagnosticMode = "openFilesOnly",
+							useLibraryCodeForTypes = true,
+						},
+					},
+				},
+			},
 			rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
@@ -117,16 +156,16 @@ return { -- LSP Configuration & Plugins
 			-- But for many setups, the LSP (`tsserver`) will work just fine
 			-- tsserver = {},
 			--
-            gitlab_ci_ls = {
-                cmd = { 'gitlab-ci-ls' },
-                init_options = {
-                    cache = '~/.cache/gitlab-ci-ls/',
-                    log_path = '~/.cache/gitlab-ci-ls/log/gitlab-ci-ls.log',
-                    options = {
-                        dependencies_autocomplete_stage_filtering = false,
-                    },
-                },
-            },
+			gitlab_ci_ls = {
+				cmd = { "gitlab-ci-ls" },
+				init_options = {
+					cache = "~/.cache/gitlab-ci-ls/",
+					log_path = "~/.cache/gitlab-ci-ls/log/gitlab-ci-ls.log",
+					options = {
+						dependencies_autocomplete_stage_filtering = false,
+					},
+				},
+			},
 
 			lua_ls = {
 				settings = {
@@ -147,13 +186,16 @@ return { -- LSP Configuration & Plugins
 		}
 
 		-- Ensure the servers and tools above are installed
-		--  To check the current status of installed tools and/or manually install
-		--  other tools, you can run
+		--
+		-- To check the current status of installed tools and/or manually install
+		-- other tools, you can run
 		--    :Mason
 		--
-		--  You can press `g?` for help in this menu.
-		require("mason").setup()
-
+		-- You can press `g?` for help in this menu.
+		--
+		-- `mason` had to be setup earlier: to configure its options see the
+		-- `dependencies` table for `nvim-lspconfig` above.
+		--
 		-- You can add other tools here that you want Mason to install
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
@@ -162,12 +204,11 @@ return { -- LSP Configuration & Plugins
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("mason-lspconfig").setup({
+			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+			automatic_installation = false,
 			handlers = {
 				function(server_name)
 					local server = servers[server_name] or {}
-					-- This handles overriding only values explicitly passed
-					-- by the server configuration above. Useful when disabling
-					-- certain features of an LSP (for example, turning off formatting for tsserver)
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 					require("lspconfig")[server_name].setup(server)
 				end,
