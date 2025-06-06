c815e38a835a14d0543d9e319f99ed9ac4a116fd
oo00oo00oo00 <oo00oo00oo00@tripleten.com>
2025-06-06 17:43:03 +0200

    fmt

--- lua/oo00oo00oo00/init.lua
+++ lua/oo00oo00oo00/init.lua
@@ -16,12 +16,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- For gitlab-ci lsp
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.gitlab-ci*.{yml,yaml}",
-  callback = function()
-    vim.bo.filetype = "yaml.gitlab"
-  end,
+	pattern = "*.gitlab-ci*.{yml,yaml}",
+	callback = function()
+		vim.bo.filetype = "yaml.gitlab"
+	end,
 })
-
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -48,38 +47,38 @@ require("lazy").setup({
 		},
 	},
 	{ "gkapfham/vim-vitamin-onec" },
-  {
-    "EdenEast/nightfox.nvim",
-    config = function()
-      require('nightfox').setup({
-        options = {
-          -- Compiled file's destination location
-          transparent = true,     -- Disable setting background
-          terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false,    -- Non focused panes set to alternative background
-          module_default = true,   -- Default enable value for modules
-          styles = {               -- Style to be applied to different syntax groups
-            comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
-            conditionals = "NONE",
-            constants = "NONE",
-            functions = "NONE",
-            keywords = "NONE",
-            numbers = "NONE",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-            variables = "NONE",
-          },
-          inverse = {             -- Inverse highlight for different types
-            match_paren = true,
-            visual = false,
-            search = true,
-          },
-        },
-      })
-      vim.cmd.colorscheme("nightfox")
-    end,
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			require("nightfox").setup({
+				options = {
+					-- Compiled file's destination location
+					transparent = true, -- Disable setting background
+					terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+					dim_inactive = false, -- Non focused panes set to alternative background
+					module_default = true, -- Default enable value for modules
+					styles = { -- Style to be applied to different syntax groups
+						comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+						conditionals = "NONE",
+						constants = "NONE",
+						functions = "NONE",
+						keywords = "NONE",
+						numbers = "NONE",
+						operators = "NONE",
+						strings = "NONE",
+						types = "NONE",
+						variables = "NONE",
+					},
+					inverse = { -- Inverse highlight for different types
+						match_paren = true,
+						visual = false,
+						search = true,
+					},
+				},
+			})
+			vim.cmd.colorscheme("nightfox")
+		end,
+	},
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
